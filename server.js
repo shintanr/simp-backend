82f367d61f8dcc941e811206298d0280463c8069
shintanr <shintanurrohmah143@gmail.com>
2025-06-22 12:49:17 +0700

    edit server.js

--- server.js
+++ server.js
@@ -2593,5 +2593,5 @@ app.post('/api/attempts/:id_attempts/submit-score', async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
