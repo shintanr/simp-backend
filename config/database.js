c2b2a016ca77123772b312f1d5895bd055dc27fe
shintanr <shintanurrohmah143@gmail.com>
2025-06-22 14:03:48 +0700

    edit mysql_url di server.js

--- config/database.js
+++ config/database.js
@@ -4,13 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT || 3306,
-})
+export const pool = mysql.createPool(process.env.MYSQL_URL);
 
 // Fungsi untuk mengambil daftar lab
 
